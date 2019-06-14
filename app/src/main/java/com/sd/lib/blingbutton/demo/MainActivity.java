4289cea12a66f5e20d787b4fa9a070fe4be343e6
zjun <565061763@qq.com>
2019-06-14 11:23:53 +0800

    demo

--- app/src/main/java/com/sd/lib/blingbutton/demo/MainActivity.java
+++ app/src/main/java/com/sd/lib/blingbutton/demo/MainActivity.java
@@ -4,20 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bling_button).setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                v.setSelected(!v.isSelected());
-            }
-        });
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        v.setSelected(!v.isSelected());
     }
 }
